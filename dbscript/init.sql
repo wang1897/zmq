2c731292ba9a709f26bc65f2faedad1d04bb1d33
wang1897 <38213362+wang1897@users.noreply.github.com>
2018-11-03 11:35:51 +0800

    Update
    Update

--- dbscript/init.sql
+++ dbscript/init.sql
@@ -37,3 +37,66 @@ CREATE TABLE `transaction` (
   KEY `index_time` (`time`),
   KEY `index_block_height` (`block_height`)
 ) ENGINE=InnoDB AUTO_INCREMENT=897521 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE block_info;
+CREATE TABLE `block_info` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `block_hash` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `block_height` int(8) DEFAULT NULL,
+  `block_size` int(8) DEFAULT NULL,
+  `block_weight` int(8) DEFAULT NULL,
+  `block_time` datetime DEFAULT NULL,
+  `block_award` decimal(16,8) DEFAULT NULL,
+  `block_merkle` varchar(80) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `block_miner` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `block_txcount` int(8) DEFAULT NULL,
+  `block_preblockhash` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique` (`block_hash`,`block_preblockhash`),
+  KEY `index_block_hash` (`block_hash`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE tx_info;
+CREATE TABLE `tx_info` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `tx_id` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `tx_vincount` int(8) DEFAULT NULL,
+    `tx_vin` longtext COLLATE utf8mb4_unicode_ci,
+    `tx_voutcount` int(8) DEFAULT NULL,
+    `tx_vout` longtext COLLATE utf8mb4_unicode_ci,
+  `time` datetime DEFAULT NULL,
+  `block_hash` varchar(120) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `block_height` int(8) DEFAULT NULL,
+  `size` int(8) DEFAULT NULL,
+  `tx_fee` decimal(16,8) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `tx_id_UNIQUE` (`tx_id`),
+  KEY `index_time` (`time`),
+  KEY `index_block_height` (`block_height`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE address_info;
+CREATE TABLE `address_info` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `address` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `block_height` int(8) DEFAULT NULL,
+  `tx_hash` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `balance_change` decimal(24,8) DEFAULT NULL,
+    `token_symbol` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `token_address` varchar(80) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_tx_hash` (`tx_hash`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+DROP TABLE token_info;
+CREATE TABLE `token_info` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `symbol` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `contract_address` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `decimal` int(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
